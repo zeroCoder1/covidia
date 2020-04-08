4ab07d77c3fccfaad130b88c997e9f636038584a
Shrutesh Sharma <shrutesh1@gmail.com>
2020-04-09 01:39:35 +0530

    added trippy shit

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 
 // Options
-obj.props.ip = 'http://94.23.252.14:8067'; // your shoutcast 2 ip with port
+obj.props.ip = 'https://uk4.internet-radio.com'; // your shoutcast 2 ip with port
 obj.props.songSize = 12; // song font size
 obj.props.bandNameSize = 18; // band name font size
 obj.props.borderRound = 20; // border round (default: 20, set it to 0, if you want no round corners)
